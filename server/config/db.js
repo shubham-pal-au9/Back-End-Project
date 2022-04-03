6a0a1d2eb1bc6d5cda875dd670f1008ca61c61cd
Shubham <spal5776@gmail.com>
2022-04-03 16:34:55 +0530

    added config file and update packages

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
-
+//const URI = "mongodb+srv://rajatbhatt123:<password>@cluster0.hdttm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
